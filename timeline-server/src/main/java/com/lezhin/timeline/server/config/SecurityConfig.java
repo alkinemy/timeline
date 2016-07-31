a32188ef3b5c6d87fe877838087e0bec060aceaa
joke111 <alta.second@gmail.com>
2016-07-31 21:17:39 +0900

    security 설정 수정

--- timeline-server/src/main/java/com/lezhin/timeline/server/config/SecurityConfig.java
+++ timeline-server/src/main/java/com/lezhin/timeline/server/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.lezhin.timeline.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 
 //@EnableWebSecurity
 //@Configuration
@@ -9,7 +11,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		http
+			.httpBasic()
+			.and()
+			.csrf().disable()
+			.antMatcher("/api/**")
+			.authorizeRequests().anyRequest().hasAuthority("ROLE_API")
+			.and()
+//			.addFilterAfter()
+			.exceptionHandling()
+			.authenticationEntryPoint(http403ForbiddenEntryPoint());
+	}
+
+	@Bean
+	private Http403ForbiddenEntryPoint http403ForbiddenEntryPoint() {
+		return new Http403ForbiddenEntryPoint();
 	}
 
 }
