540ecce0c4887267db1336be7b1a005cfba023fe
joke111 <alta.second@gmail.com>
2016-08-04 01:13:16 +0900

    유저 이동 뷰 추가

--- timeline-client/src/main/java/com/lezhin/timeline/client/domain/user/dto/ActivityLogDto.java
+++ timeline-client/src/main/java/com/lezhin/timeline/client/domain/user/dto/ActivityLogDto.java
@@ -11,6 +11,7 @@ public class ActivityLogDto {
 
 	private TimelineUserDto from;
 	private String message;
+	//TODO link url 생성 방식 수정 필요 -> DB에 저장하면 안될것같음, data 필드를 놓고 json으로 저장하는게 나을듯...
 	private String linkUrl;
 	private LocalDateTime createdDate;
 
